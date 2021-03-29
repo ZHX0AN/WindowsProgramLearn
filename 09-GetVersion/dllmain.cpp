b6aedf34f834b1d0b614440cfbb9bc2c429aea38
zhangjx00 <75248396@qq.com>
2021-03-29 13:58:23 +0800

    增加获取版本

---
+++ 09-GetVersion/dllmain.cpp
@@ -0,0 +1,170 @@
+﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+#include "pch.h"
+#include "resource.h"
+#include "windows.h" 
+#include "shellapi.h"
+#include <string>
+#include <tchar.h> 
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <iomanip>
+#include <strstream>
+#pragma comment(lib,"Version.lib")
+
+using namespace std;
+
+//声明函数
+VOID ShowDemoUI(HMODULE hModule);
+INT_PTR CALLBACK DialogProc(_In_ HWND   hwndDlg, _In_ UINT   uMsg, _In_ WPARAM wParam, _In_ LPARAM lParam);
+
+VOID GetVersion(HWND hwndDlg);
+VOID UnLoadMyself();
+
+wchar_t* StrToWchar(std::string str);
+
+////定义变量
+DWORD wxBaseAddress = 0;
+
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+	{
+		HANDLE hANDLE = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)ShowDemoUI, hModule, NULL, 0);
+		if (hANDLE != 0)
+		{
+			CloseHandle(hANDLE);
+		}
+		break;
+	}
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
+
+
+
+
+//显示操作的窗口
+VOID ShowDemoUI(HMODULE hModule)
+{
+	//获取WeChatWin.dll的基址
+	wxBaseAddress = (DWORD)GetModuleHandle(TEXT("WeChatWin.dll"));
+	DialogBox(hModule, MAKEINTRESOURCE(IDD_MAIN), NULL, &DialogProc);
+}
+
+//窗口回调函数，处理窗口事件
+INT_PTR CALLBACK DialogProc(_In_ HWND   hwndDlg, _In_ UINT   uMsg, _In_ WPARAM wParam, _In_ LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+		break;
+
+	case WM_CLOSE:
+		//关闭窗口事件
+		EndDialog(hwndDlg, 0);
+		break;
+	case WM_COMMAND:
+
+		if (wParam == BTN_GET_VERSION)
+		{
+			GetVersion(hwndDlg);
+		}
+		else if (wParam == IDC_BTN_UNLOAD) {
+			UnLoadMyself();
+		}
+		break;
+	default:
+		break;
+	}
+	return FALSE;
+}
+
+
+
+
+VOID GetVersion(HWND hwndDlg)
+{
+
+	WCHAR VersionFilePath[MAX_PATH];
+	if (GetModuleFileName((HMODULE)wxBaseAddress, VersionFilePath, MAX_PATH) == 0)
+	{
+		return;
+	}
+
+	string asVer = "";
+	VS_FIXEDFILEINFO* pVsInfo;
+	unsigned int iFileInfoSize = sizeof(VS_FIXEDFILEINFO);
+	int iVerInfoSize = GetFileVersionInfoSize(VersionFilePath, NULL);
+	if (iVerInfoSize != 0) {
+		char* pBuf = new char[iVerInfoSize];
+		if (GetFileVersionInfo(VersionFilePath, 0, iVerInfoSize, pBuf)) {
+			if (VerQueryValue(pBuf, TEXT("\\"), (void**)&pVsInfo, &iFileInfoSize)) {
+				//主版本2.6.7.57
+				//2
+				int s_major_ver = (pVsInfo->dwFileVersionMS >> 16) & 0x0000FFFF;
+				//6
+				int s_minor_ver = pVsInfo->dwFileVersionMS & 0x0000FFFF;
+				//7
+				int s_build_num = (pVsInfo->dwFileVersionLS >> 16) & 0x0000FFFF;
+				//57
+				int s_revision_num = pVsInfo->dwFileVersionLS & 0x0000FFFF;
+
+				//把版本变成字符串
+				strstream wxVer;
+				wxVer << s_major_ver << "." << s_minor_ver << "." << s_build_num << "." << s_revision_num;
+				wxVer >> asVer;
+			}
+		}
+		delete[] pBuf;
+	}
+
+
+
+	SetWindowText(GetDlgItem(hwndDlg, IDC_TEXT_INFO), StrToWchar(asVer));
+}
+
+
+
+
+//卸载自己
+VOID UnLoadMyself()
+{
+	HMODULE hModule = NULL;
+
+	//GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS 会增加引用计数
+	//因此，后面还需执行一次FreeLibrary
+	//直接使用本函数（UnInject）地址来定位本模块
+	GetModuleHandleEx(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS, (LPWSTR)&UnLoadMyself, &hModule);
+
+	if (hModule != 0)
+	{
+		//减少一次引用计数
+		FreeLibrary(hModule);
+
+		//卸载自己并退出
+		FreeLibraryAndExitThread(hModule, 0);
+	}
+}
+
+
+
+
+wchar_t* StrToWchar(std::string str)
+{
+	int strSize = (int)(str.length() + 1);
+	wchar_t* wStr = new wchar_t[strSize];
+	MultiByteToWideChar(CP_ACP, 0, str.c_str(), -1, wStr, strSize);
+	return wStr;
+	delete[] wStr;
+}
