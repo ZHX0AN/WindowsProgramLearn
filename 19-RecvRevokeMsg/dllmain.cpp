9c974dc31b4fb69b4412f676eeb5cd14d1348dfc
zhangjx00 <75248396@qq.com>
2021-04-09 14:44:04 +0800

    修改消息撤回

--- 19-RecvRevokeMsg/dllmain.cpp
+++ 19-RecvRevokeMsg/dllmain.cpp
@@ -32,6 +32,7 @@ wchar_t* GetMsgByAddress2(DWORD memAddress);
 VOID RecvRevokeHookWx();
 VOID RecvRevokeInlineHook();
 VOID RecvRevokeDealMsg(DWORD r_edx);
+VOID RecvRevokeDealMsg2(DWORD r_ecx, DWORD r_edx);
 
 ////定义变量
 DWORD wxBaseAddress = 0;
@@ -148,9 +149,16 @@ __declspec(naked) VOID RecvRevokeInlineHook()
 
 
 		//调用接收消息的函数
+		//方法一
+		//push edx
+		//call RecvRevokeDealMsg
+		//add esp, 4
+
+		//方法二
 		push edx
-		call RecvRevokeDealMsg
-		add esp, 4
+		push ecx
+		call RecvRevokeDealMsg2
+		add esp, 8
 
 		popf
 		popad
@@ -163,6 +171,59 @@ __declspec(naked) VOID RecvRevokeInlineHook()
 VOID RecvRevokeDealMsg(DWORD r_edx)
 {
 	/*地址跟接收消息一样*/
+
+	DWORD SvridOffset = 0x28;
+	//时间戳
+	DWORD timestampOffset = 0x3C;
+
+	//消息类型
+	DWORD msgTypeOffset = 0x30;
+	//发送人，好友或群ID
+	DWORD friendOffset = 0x40;
+	//群消息，消息发送者
+	DWORD roomMsgSenderOffset = 0x164;
+	//消息内容
+	DWORD contentOffset = 0x68;
+
+	//子类型，如果是文件，sub=6
+	DWORD subTypeOffset = 0xEC;
+
+	//好友消息：<msgsource />
+	//群消息: 01023B78  <msgsource>..<silence>0</silence>..<membercount>2</membercount> < / msgsource>
+	//群at消息: <msgsource>..<atuserlist>wxid_4sy2barbyny712</atuserlist>..<silence>0</silence>..<membercount>2</membercount>.</msgsource>
+	DWORD atFriendOffst = 0x1b8;
+
+	//文件存储路径：wxid_4sy2barbyny712\FileStorage\File\2021-03\工作簿1(1).xlsx
+	DWORD filePathOffset = 0x1A0;
+
+
+	INT64 Svrid = *((INT64*)(r_edx + SvridOffset));
+	DWORD msgType = *((DWORD*)(r_edx + msgTypeOffset));
+	DWORD msgSubType = *((DWORD*)(r_edx + subTypeOffset));
+	DWORD timestamp = *((DWORD*)(r_edx + timestampOffset));
+	wchar_t* wToWxId = GetMsgByAddress2(r_edx + friendOffset);
+	wchar_t* wContent = GetMsgByAddress2(r_edx + contentOffset);
+	wchar_t* wAt = GetMsgByAddress2(r_edx + atFriendOffst);
+	wchar_t* wFilePath = GetMsgByAddress2(r_edx + filePathOffset);
+	wchar_t* wRoomSender = GetMsgByAddress2(r_edx + roomMsgSenderOffset);
+
+	AddText(GetDlgItem(g_hwndDlg, IDC_TEXT_INFO), TEXT("Svrid: %I64d \r\n"), Svrid);
+	AddText(GetDlgItem(g_hwndDlg, IDC_TEXT_INFO), TEXT("时间戳: %I32d \r\n"), timestamp);
+	AddText(GetDlgItem(g_hwndDlg, IDC_TEXT_INFO), TEXT("信息类型: %d \r\n"), msgType);
+	AddText(GetDlgItem(g_hwndDlg, IDC_TEXT_INFO), TEXT("信息子类型: %d \r\n"), msgSubType);
+	AddText(GetDlgItem(g_hwndDlg, IDC_TEXT_INFO), TEXT("接收者: %s \r\n"), wToWxId);
+	AddText(GetDlgItem(g_hwndDlg, IDC_TEXT_INFO), TEXT("消息内容: %s \r\n"), wContent);
+	AddText(GetDlgItem(g_hwndDlg, IDC_TEXT_INFO), TEXT("AT好友: %s \r\n"), wAt);
+	AddText(GetDlgItem(g_hwndDlg, IDC_TEXT_INFO), TEXT("文件路径: %s \r\n"), wFilePath);
+	AddText(GetDlgItem(g_hwndDlg, IDC_TEXT_INFO), TEXT("群消息发送者: %s \r\n"), wRoomSender);
+
+
+
+
+}
+
+VOID RecvRevokeDealMsg2(DWORD r_ecx, DWORD r_edx)
+{
 
 	DWORD SvridOffset = 0x28;
 	//时间戳
@@ -209,7 +270,28 @@ VOID RecvRevokeDealMsg(DWORD r_edx)
 	AddText(GetDlgItem(g_hwndDlg, IDC_TEXT_INFO), TEXT("AT好友: %s \r\n"), wAt);
 	AddText(GetDlgItem(g_hwndDlg, IDC_TEXT_INFO), TEXT("文件路径: %s \r\n"), wFilePath);
 	AddText(GetDlgItem(g_hwndDlg, IDC_TEXT_INFO), TEXT("群消息发送者: %s \r\n"), wRoomSender);
+
+	AddText(GetDlgItem(g_hwndDlg, IDC_TEXT_INFO), TEXT(" *************** 下面是撤回消息 ************** \r\n"));
+
+
+	DWORD recv_SvridOffset = 0x28;
+	DWORD recv_TimestampOffset = 0x28;
+	DWORD recv_ContentOffset = 0x70;
+	DWORD recv_MsgFromWxidOffset = 0x98;//消息来源，群ID或好友ID
+
+	INT64 recv_SvridOff = *((INT64*)(r_ecx + recv_SvridOffset));
+	DWORD recv_timestamp = *((DWORD*)(r_ecx + recv_TimestampOffset));
+	wchar_t* recv_wContent = GetMsgByAddress2(r_ecx + recv_ContentOffset);
+	wchar_t* recv_wMsgFromWxidWxid = GetMsgByAddress2(r_ecx + recv_MsgFromWxidOffset);
+	AddText(GetDlgItem(g_hwndDlg, IDC_TEXT_INFO), TEXT("Svrid: %I64d \r\n"), recv_SvridOff);
+	AddText(GetDlgItem(g_hwndDlg, IDC_TEXT_INFO), TEXT("时间戳: %I32d \r\n"), recv_timestamp);
+	AddText(GetDlgItem(g_hwndDlg, IDC_TEXT_INFO), TEXT("消息内容: %s \r\n"), recv_wContent);
+	AddText(GetDlgItem(g_hwndDlg, IDC_TEXT_INFO), TEXT("撤回消息者: %s \r\n"), recv_wMsgFromWxidWxid);
+
+
+
 }
+
 
 
 VOID AddText(HWND hwnd, PCTSTR pszFormat, ...) {
